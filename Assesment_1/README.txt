9cf2d51456de91e8b75f9f7c18f6817e4ae0e249
Subbaramaiah Chevuru <ram.subaram@gmail.com>
2024-08-04 09:12:09 +0530

    Assessment-1 Program for pack and unpack the data
    Activity: Advanced_Erlang_Training

---
+++ Assesment_1/README.txt
@@ -0,0 +1,3 @@
+
+Try representing a house using a tuple and a street using a list of houses. Make sure you can pack and unpack the data in the representations. 
+
---
+++ Assesment_1/assessment_1.erl
@@ -0,0 +1,50 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%								%%%
+%%%    Assesment_1_ADVANCED_ERLANG				%%%
+%%%								%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%								%%%
+%%%			REVISION LOG				%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%								%%%
+%%% 04/Aug/2024	Subbu	Initial Draft for the Pack and Unpack	%%%
+%%%			the data from the tuples and Lists.	%%%
+%%%								%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(assessment_1).
+-export([create_house/3, create_street/1, unpack_house/1, unpack_street/1,start/0]).
+
+% Define a house as a tuple with address, number of bedrooms, and number of bathrooms
+create_house(Address, Bedrooms, Bathrooms) ->
+    {Address, Bedrooms, Bathrooms}.
+
+% Define a street as a list of houses
+create_street(Houses) ->
+    Houses.
+
+% Unpack the data from a house tuple
+unpack_house(House) ->
+    {Address, Bedrooms, Bathrooms} = House,
+    io:format("Address: ~p, Bedrooms: ~p, Bathrooms: ~p~n", [Address, Bedrooms, Bathrooms]).
+
+% Unpack the data from the street list
+unpack_street(Street) ->
+    lists:foreach(fun(House) ->
+        {Address, Bedrooms, Bathrooms} = House,
+        io:format("Address: ~p, Bedrooms: ~p, Bathrooms: ~p~n", [Address, Bedrooms, Bathrooms])
+    end, Street).
+
+
+start() ->
+    House1 = create_house("123 Main St", 3, 2),
+    House2 = create_house("456 Elm St", 4, 3),
+    House3 = create_house("1c/2-72, West Street", 2, 1),
+    Street = create_street([House1, House2, House3]),
+    io:fwrite("~n Printing House Details: ~n"),
+    unpack_house(House1),
+    io:fwrite("~n Printing Street Details: ~n"),
+    unpack_street(Street).
